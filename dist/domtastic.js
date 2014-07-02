210b6102c76f596b3d6511fd9c3a50bf7743da4d
travis-ci <travis@travis-ci.org>
2014-07-02 07:58:21 +0000

    Update all the things

--- dist/domtastic.js
+++ dist/domtastic.js
@@ -30,7 +30,7 @@ if (typeof selector !== 'undefined') {
 extend($, contains, mode, noconflict, type);
 extend(api, array, attr, class_, css, data, dom, dom_extra, event, html, ready, selector_extra);
 extend(apiNodeList, array);
-$.version = '0.7.4';
+$.version = '0.7.5';
 $.extend = extend;
 $.fn = api;
 $.fnList = apiNodeList;
